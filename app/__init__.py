ac92076adbccb61e72f658b1137e85693732a4d9
rajd21-web <usarmah2017@gmail.com>
2026-06-16 21:50:25 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,65 +10,64 @@ from .db import db, VisitorStats
 migrate = Migrate(render_as_batch=True)
 
 def create_app():
-app = Flask(name)
+    app = Flask(__name__)
 
-app.config["SECRET_KEY"] = os.environ.get(
-    "SECRET_KEY", "NOTHING_IS_SECRET"
-)
-
-db_uri = os.environ.get(
-    "DB_URI", "sqlite:///master.sqlite3"
-)
-
-if db_uri.startswith("mysql://"):
-    db_uri = db_uri.replace(
-        "mysql://",
-        "mysql+pymysql://",
-        1
-    )
-
-app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
-app.config["PERMANENT_SESSION_LIFETIME"] = datetime.timedelta(days=7)
-
-db.init_app(app)
-migrate.init_app(app, db)
-
-@app.after_request
-def after_request_(response):
-    if request.endpoint != "static":
-        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    return response
-
-from .views.auth import auth
-from .views.home import home
-from .views.settings import settings
-from .views.admin import admin
-
-blueprints = [auth, home, settings, admin]
-
-@app.before_request
-def app_before_data():
-    if request.endpoint != "static":
-        user_agent = parse(request.user_agent.string)
-
-        browser = user_agent.browser.family
-        device = user_agent.get_device()
-        operating_system = user_agent.get_os()
-        bot = user_agent.is_bot
-
-        stat = VisitorStats(
-            browser=browser,
-            device=device,
-            operating_system=operating_system,
-            is_bot=bot
+        app.config["SECRET_KEY"] = os.environ.get(
+            "SECRET_KEY", "NOTHING_IS_SECRET"
         )
 
-        db.session.add(stat)
-        db.session.commit()
+        db_uri = os.environ.get(
+        "DB_URI", "sqlite:///master.sqlite3"
+        )
 
-for bp in blueprints:
-    app.register_blueprint(bp)
+        if db_uri.startswith("mysql://"):
+            db_uri = db_uri.replace(
+                "mysql://",
+                "mysql+pymysql://",
+                1
+            )
 
-app.cli.add_command(create_admin_user)
+        app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+        app.config["PERMANENT_SESSION_LIFETIME"] = datetime.timedelta(days=7)
 
-return app
+        db.init_app(app)
+        migrate.init_app(app, db)
+
+        @app.after_request
+        def after_request_(response):
+            if request.endpoint != "static":
+                response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+            return response
+
+        from .views.auth import auth
+        from .views.home import home
+        from .views.settings import settings
+        from .views.admin import admin
+
+        blueprints = [auth, home, settings, admin]
+
+        @app.before_request
+        def app_before_data():
+            if request.endpoint != "static":
+                user_agent = parse(request.user_agent.string)
+
+                browser = user_agent.browser.family
+                device = user_agent.get_device()
+                operating_system = user_agent.get_os()
+                bot = user_agent.is_bot
+
+                stat = VisitorStats(
+                    browser=browser,
+                    device=device,
+                    operating_system=operating_system,
+                    is_bot=bot
+                )
+
+                db.session.add(stat)
+                db.session.commit()
+
+        for bp in blueprints:
+            app.register_blueprint(bp)
+
+        app.cli.add_command(create_admin_user)
+        return app
